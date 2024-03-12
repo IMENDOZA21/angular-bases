933317d436b3fe6c5bb755f52d181888e5e55565
IMENDOZA <imendoza@unab.edu.co>
2024-03-12 10:40:51 -0500

    ADD - New module DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     AppRoutingModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+
+
+
+@NgModule({
+  declarations: [ MainPageComponent ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MainPageComponent
+  ]
+})
+export class DbzModule { }
