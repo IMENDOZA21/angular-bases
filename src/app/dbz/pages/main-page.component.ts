43d09b0e64f78dc8700c54b81d2f6e7c48c3a1bc
IMENDOZA <imendoza@unab.edu.co>
2024-03-14 16:28:30 -0500

    CHANGE - Implementacion de buenas practicas al dejar el servicio como privado e implementar metodos de acceso

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -9,6 +10,18 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent {
 
-  constructor( public dbzService: DbzService) {
+  constructor( private dbzService: DbzService) {
+  }
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter( character: Character): void {
+    this.dbzService.addCharacter(character);
   }
 }
